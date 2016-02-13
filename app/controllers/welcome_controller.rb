edcf3ed1b419dd16678e18bf7586d38a2eab14cc
vagrant <vagrant@vagrant-centos64.vagrantup.com>
2016-02-13 13:57:19 +0000

    Primera subida

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+	@textos = Tabla1.all
+#	respond_to do |format|
+#		format.html  # index.html.erb
+#		format.json  { render :json => @textos }
+#	end
   end
 end
---
+++ app/models/tabla1.rb
@@ -0,0 +1,3 @@
+class Tabla1 < ActiveRecord::Base
+end
+
